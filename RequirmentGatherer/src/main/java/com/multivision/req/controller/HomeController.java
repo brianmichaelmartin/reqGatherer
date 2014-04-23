43051b9b2519c72c5d1c236424e2bde447c0db54
brianmichaelmartin <brian.michael.martin@gmail.com>
2014-04-23 11:26:26 -0400

    goodbye

--- RequirmentGatherer/src/main/java/com/multivision/req/controller/HomeController.java
+++ RequirmentGatherer/src/main/java/com/multivision/req/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.multivision.req.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
+ * GOOD LUCK YOU FUCKERS
  */
 @Controller
 public class HomeController {
@@ -22,16 +21,11 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
